1bd484eacbc6100ce29a9640a4d3bdbdb2c0f21c
Luciana <lucyfierceit@gmail.com>
2022-10-05 22:40:02 -0300

    paginaprodutos

--- controllers/baixoControllers.js
+++ controllers/baixoControllers.js
@@ -1,10 +1,21 @@
 const path = require('path')
+const produtos = require('../public/js/produtos')
 
 const baixoControllers = {
     index: (req, res) => {
-        
+        let categoria = req.query.categoria
+        console.log(categoria)
+        console.log(produtos)
 
-        res.render('baixo')
+        if (!categoria)
+        res.render('baixo', {produtos: produtos})
+        
+        else{
+            let resultado = produtos.filter(p => p.category == categoria) 
+        res.render('baixo',{produtos : resultado})
+
+        }
+
     }
 }
 module.exports = baixoControllers
---
+++ public/js/produtos.js
@@ -0,0 +1,131 @@
+
+
+const produtos = [
+      {
+        category: 1,
+        id: 1,
+        name: "Sire v1",
+        price: 1200,
+        description: "lorem ipsum dolor sit amet",
+        colors: [
+          {
+            name: "red",
+            img: "/img/Baixos/Sire Marcus Miller V3 2nd-amarelo.png",
+            main: true
+          }
+        ]
+      },
+      {
+        category: 1,
+        id: 2,
+        name: "Sire v2",
+        price: 3123,
+        description: "lorem ipsum dolor sit amet",
+        colors: [
+          {
+            name: "red",
+            img: "/img/baixos/sirev2.jpeg",
+            main: true
+          }
+        ]
+      },
+      {
+        category: 1,
+        id: 3,
+        name: "Sire v3",
+        price: 3422,
+        description: "lorem ipsum dolor sit amet",
+        colors: [
+          {
+            name: "red",
+            img: "/img/baixos/sirev3.jpeg",
+            main: true
+          }
+        ]
+      },
+      {
+        category: 2,
+        id: 1,
+        name: "Sire g1",
+        price: 2321,
+        description: "lorem ipsum dolor sit amet",
+        colors: [
+          {
+            name: "red",
+            img: "/img/guitarras/sireg1.jpeg",
+            main: true
+          }
+        ]
+      },
+      {
+        category: 2,
+        id: 2,
+        name: "Sire g2",
+        price: 4321,
+        description: "lorem ipsum dolor sit amet",
+        colors: [
+          {
+            name: "red",
+            img: "/img/guitarras/sireg2.jpeg",
+            main: true
+          }
+        ]
+      },
+      {
+        category: 2,
+        id: 3,
+        name: "Sire g3",
+        price: 2314,
+        description: "lorem ipsum dolor sit amet",
+        colors: [
+          {
+            name: "red",
+            img: "/img/guitarras/sireg3.jpeg",
+            main: true
+          }
+        ]
+      },
+      {
+        category: 3,
+        id: 1,
+        name: "Sire a1",
+        price: 4412,
+        description: "lorem ipsum dolor sit amet",
+        colors: [
+          {
+            name: "red",
+            img: "/img/violoes/sireg1.jpeg",
+            main: true
+          }
+        ]
+      },
+      {
+        category: 3,
+        id: 2,
+        name: "Sire a2",
+        price: 3212,
+        description: "lorem ipsum dolor sit amet",
+        colors: [
+          {
+            name: "red",
+            img: "/img/violoes/sireg2.jpeg",
+            main: true
+          }
+        ]
+      },
+      {
+        category: 3,
+        id: 3,
+        name: "Sire a3",
+        price: 3123,
+        description: "lorem ipsum dolor sit amet",
+        colors: [
+          {
+            name: "red",
+            img: "/img/violoes/sireg3.jpeg",
+            main: true
+          }
+        ]
+      }
+    ]
+module.exports = produtos
