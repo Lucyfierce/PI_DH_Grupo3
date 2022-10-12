357a370bc3998b755fbfc7bb77ad7ed1dfacfc95
Mônica Silva <monica.flor.silva@gmail.com>
2022-10-12 13:54:37 -0300

    ajuste footer e rota sobre nos

--- routes/sobrenos.js
+++ routes/sobrenos.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const sobrenosControllers = require('../controllers/indexControllers')
+const sobrenosControllers = require('../controllers/sobrenosControllers')
 router.get('/', sobrenosControllers.index)
 
 module.exports = router
