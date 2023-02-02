9a578ec60beb95cc2738ba62de49e1ab13d182a4
claudiusCCS <claudiuslapidar@gmail.com>
2023-02-02 20:46:57 -0300

    requisicao-Apis

--- controllers/produtosControllers.js
+++ controllers/produtosControllers.js
@@ -1,48 +1,114 @@
 const path = require('path')
 const fs = require('fs');
+const produtoRequest = require('../requests/produtoRequest')
 
-const produtoJson = path.join(__dirname,'../products.json');
+const produtoJson = path.join(__dirname, '../products.json');
 const produtos = JSON.parse(fs.readFileSync(produtoJson))
 
 const produtosControllers = {
     index: (req, res) => {
-        
-        let categoria = req.query.categoria
+        produtoRequest.getProduto()
+            .then((protudosRetornados) => {
+                protudosRetornados.data;
+                let categoria = req.query.categoria
 
-        //
-        
-        if (!categoria)
-            res.render('produtos', { produtos: produtos })
+                //
 
-        else {
-            let resultado = produtos.filter(p => p.categoria == categoria)
-            res.render('produtos', { produtos: resultado })
+                if (!categoria)
+                    res.render('produtos', { produtos: produtos })
 
-        }
-    
+                else {
+                    let resultado = produtos.filter(p => p.categoria == categoria)
+                    res.render('produtos', { produtos: resultado })
+
+                }
+            })
+            .catch((err) => {
+                //console.log(err)
+                res.render('error', { msg: "Produto não encontrado", err: err });
+            })
     },
 
     //Edit
     edit: (req, res) => {
-    let id = req.params.id
-    let productToEdit = products.find(product => product.id == id)
-    res.render('pageProductEdit', {productToEdit})
+        let id = req.params.id
+        let productToEdit = products.find(product => product.id == id)
+        produtoRequest.getProduto(id)
+            .then((protudosRetornados) => {
+                protudosRetornados.data
+                res.render('pageProductEdit', { productToEdit })
+            })
+            .catch((err) => {
+                // console.log(error)
+                res.render('error', { msg: "Produto não encontrado", err: err });
+            })
     },
     //Update POST
     update: (req, res) => {
-    let id = req.params.id;
-    let productToEdit = products.find(product => product.id == id)
-    let image;
-    if (req.files[0] != undefined){
-        image = req.files [0].filename
-    }else{
-        image = productToEdit.image
+        let id = req.params.id;
+        let productToEdit = products.find(product => product.id == id)
+        let image;
+
+        produtoRequest.updateProduto(id)
+            .then((protudosRetornados) => {
+
+                protudosRetornados.data
+                res.render('pageProductEdit', { productToEdit })
+            })
+        if (req.files[0] != undefined) {
+            image = req.files[0].filename
+        } else {
+            image = productToEdit.image
+        }
+        productToEdit = {
+            id: productToEdit.id,
+            ...req.body,
+            image: image,
+        }
+
+    },
+
+    //Create
+    create: (req, res) => {
+        const msgSucesso = "Produto criado com sucesso!"
+        const msgFalha = "Produto não criado. Tente novamente."
+        let user = {
+            nome: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            newsLetter: req.body.newsLetter
+        }
+        let produto = user
+        produtoRequest.createProduto(produto)
+
+            .then((produtoExiste) => {
+                if (produtoExiste != undefined) {
+                    res.render('produtos', json({ msg: msgSucesso }))
+                }
+            })
+            .catch(err => {
+                res.render('error', json({ msg: msgFalha, err: err }));
+            })
+    },
+
+    //DELETE
+    destroy: (req, res) => {
+        let id = req.params.id
+
+        produtoRequest.deleteProduto(id)
+
+            .then((protudoRetornado) => {
+
+                Produto.destroy({ where: { id: id } })
+                protudoRetornado.data
+
+                res.render(json({ msg: 'Produto excluído com sucesso!' }))
+            })
+            .catch((err) => {
+                // console.log(error)
+                res.render('error', { msg: "Produto não encontrado!", err: err });
+            })
     }
-    productToEdit = {
-        id: productToEdit.id,
-        ...req.body,
-        image: image,
-    }}
 
 }
 module.exports = produtosControllers;
---
+++ requests/produtoRequest.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const def = require('./default');
+
+const url = "produto"; //contexto de negocio
+
+const produtoRequest = {
+
+    getProduto: () => axios({
+        ...def,
+        method: 'get',
+        url: `${url}/`
+    }),
+    
+    getProduto: (id) => axios({
+        ...def,
+        method: 'get',
+        url: `${url}/${id}`
+    }),
+
+    createProduto: () => axios({
+        ...def,
+        method: 'post',
+        data: {
+            ...produto
+        },
+        url: `${url}/`
+    }),
+
+    updateProduto: (id) => axios({
+        ...def,
+        method: 'update',
+        url: `${url}/${id}`
+    }),
+
+    deleteProduto: (id) => axios({
+        ...def,
+        method: 'delete',
+        url: `${url}/${id}`
+    })
+};
+
+module.exports = produtoRequest;
